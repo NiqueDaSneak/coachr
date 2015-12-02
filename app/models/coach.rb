8aad3f48f4d946cef46119df2005cc90c52bf428
NiqueDaSneak <niquedasneak4@gmail.com>
2015-12-02 12:56:07 -0500

    work on building views and controller skeleton to build on top of

--- app/models/coach.rb
+++ app/models/coach.rb
@@ -1,4 +1,9 @@
 class Coach < ActiveRecord::Base
+  validates :full_name, presence: true, uniqueness: true
+  validates :email, email: true, uniqueness: true
+
   has_many :clients
+
+  has_secure_password
 
 end

f7e2c9af2587d8a06cf8b284c8e6552167c1e26c
NiqueDaSneak <niquedasneak4@gmail.com>
2015-12-02 18:26:33 -0500

    create logged_in methods for both clients and coachs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,20 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def current_user
-    @current_user ||= User.find_by(id: session[:id]) if session[:id]
+  def current_coach
+    @current_coach ||= Coach.find_by(id: session[:id]) if session[:id]
   end
 
-  def logged_in?
-    !!current_user
+  def current_client
+    @current_client ||= Client.find_by(id: session[:id]) if session[:id]
+  end
+
+  def coach_logged_in?
+    !!current_coach
+  end
+
+  def client_logged_in?
+    !!current_client
   end
 
   helper_method :current_user, :logged_in?

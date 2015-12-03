775a90a5649283d841249602df836fb7a10b2dee
NiqueDaSneak <niquedasneak4@gmail.com>
2015-12-03 14:02:57 -0500

    add foundation for sending email on client creation

---
+++ app/mailers/application_mailer.rb
@@ -0,0 +1,4 @@
+class ApplicationMailer < ActionMailer::Base
+  default from: "niquedasneak4@gmail.com"
+  layout 'mailer'
+end
---
+++ app/mailers/client_mailer.rb
@@ -0,0 +1,9 @@
+class ClientMailer < ApplicationMailer
+
+  def welcome_email(client)
+    @client = client
+    @url = "http://localhost:3000/client/login"
+    mail(to: @client.email, subject: "#{@client.coach.full_name} Added You On Coachr")
+  end
+
+end

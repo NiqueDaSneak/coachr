c9700f001c53d67996485d1bdc9eb1ed70924cb7
NiqueDaSneak <niquedasneak4@gmail.com>
2015-12-03 13:57:23 -0500

    build create method logic, set a global variable for random password generation; used in view and in activemailer view that is sent to client on creation

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,12 +1,26 @@
 class ClientsController < ApplicationController
 
-a
+  def new
+    @client = Client.new
+  end
+
+  def create
+    $temp_pass = password_gen
+    @client = Client.create(client_params)
+    if @client.save
+      ClientMailer.welcome_email(@client).deliver_now
+      redirect_to client_path(@client)
+    else
+      redirect_to new_client_path, :flash => {:error => @client.errors.full_messages}
+    end
+  end
+
   def login
   end
 
   private
 
   def client_params
-    client_params = params.require(:coach).permit(:full_name, :email, :password, :coach_id)
+    client_params = params.require(:client).permit(:full_name, :email, :password, :coach_id)
   end
 end

96b797395b1b4a8dc20ddd46025f8d7cd7424d56
NiqueDaSneak <niquedasneak4@gmail.com>
2015-12-03 17:57:18 -0500

    delete validates_confirmation_of

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,7 +1,6 @@
 class Client < ActiveRecord::Base
   validates :full_name, presence: true, uniqueness: true
   validates :email, email: true, uniqueness: true
-  validates_confirmation_of :password
 
   belongs_to :coach
   has_many :goals

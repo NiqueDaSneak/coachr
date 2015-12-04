df4977042e0f74e07bdc20e0ea26756a9f3c934e
NiqueDaSneak <niquedasneak4@gmail.com>
2015-12-03 21:49:07 -0500

    change app name to Clarity

--- app/mailers/client_mailer.rb
+++ app/mailers/client_mailer.rb
@@ -3,7 +3,7 @@ class ClientMailer < ApplicationMailer
   def welcome_email(client)
     @client = client
     @url = "http://localhost:3000/client/login"
-    mail(to: @client.email, subject: "#{@client.coach.full_name} Added You On Coachr")
+    mail(to: @client.email, subject: "#{@client.coach.full_name} Added You On Clarity")
   end
 
 end
